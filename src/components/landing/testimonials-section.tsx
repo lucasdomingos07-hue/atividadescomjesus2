ab1ba8ee6210ab05f062da816b5a62900ca6647e
Arthur Macêdo <thurmacedo1@gmail.com>
2025-11-08 03:19:57 +0000

    Coloque ":" no final do titulo "O que as famílias estão dizendo"

--- src/components/landing/testimonials-section.tsx
+++ src/components/landing/testimonials-section.tsx
@@ -47,7 +47,7 @@ export default function TestimonialsSection() {
     <SectionWrapper className="pt-8 pb-8">
       <div className="text-center space-y-4 mb-4">
         <h2 className="text-xl font-headline font-bold text-headline">
-          O que as famílias estão dizendo
+          O que as famílias estão dizendo:
         </h2>
       </div>
 
