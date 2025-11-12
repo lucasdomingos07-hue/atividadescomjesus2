0daaa01ec27d0ace7fd913eee16d1d31353a346b
Lucas Domingos <lucasdomingos07@gmail.com>
2025-11-12 18:05:22 +0000

    Coloque o card "O kit colorindo Fé e alegria inclui:" para cima do card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,8 +23,8 @@ export default function Home() {
         <TestimonialsSection />
         <CreatorSection />
         <BenefitsSection />
-        <PricingSection />
         <WhatYouGetSection />
+        <PricingSection />
         <FaqSection />
         <FinalCtaSection />
       </main>
