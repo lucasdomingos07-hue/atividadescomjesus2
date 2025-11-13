b2276926374696c310df398d5265a80d1270b535
Lucas Domingos <lucasdomingos07@gmail.com>
2025-11-13 01:48:22 +0000

    Troque por:
    "Atenção: As telas estão
    roubando e destruindo
    a atenção da

--- src/components/landing/hero-section.tsx
+++ src/components/landing/hero-section.tsx
@@ -16,9 +16,9 @@ export default function HeroSection() {
         <div className="flex flex-col space-y-6 text-center md:items-center">
           <div className="space-y-4 text-center">
             <h1 className="text-2xl md:text-3xl font-headline tracking-tight text-foreground text-[26px] leading-tight md:leading-snug text-glow">
-              Sente que as telas estão<br />
-              <strong>roubando e destruindo</strong> a<br />
-              atenção das crianças?
+              Atenção: As telas estão<br />
+              <strong>roubando e destruindo</strong><br />
+              a atenção das crianças!
             </h1>
             <h2 className="text-lg md:text-xl text-foreground max-w-3xl mx-auto">
               Nessas férias, você pode transformar isso em tempo de fé, conversa e conexão real. <br/><strong>+100 atividades bíblicas prontas para imprimir e colorir 🎨</strong>
