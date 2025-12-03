5ec9afbeb882560d4e9250edd699d991ba9d27b4
Lucas Domingos <lucasdomingos07@gmail.com>
2025-12-03 03:52:57 +0000

    Adicione o pixel a seguir na minha esturtura tendo a certeza que nao ira

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,13 +30,13 @@ export default function RootLayout({
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '793564009160721');
+            fbq('init', '848711979093727');
             fbq('track', 'PageView');
           `}
         </Script>
         <noscript>
           <img height="1" width="1" style={{display: 'none'}}
-          src="https://www.facebook.com/tr?id=793564009160721&ev=PageView&noscript=1"
+          src="https://www.facebook.com/tr?id=848711979093727&ev=PageView&noscript=1"
           />
         </noscript>
       </head>
