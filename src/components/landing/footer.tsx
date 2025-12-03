482ad2dcf78fbde0fcc1350f35fb280033d87516
Lucas Domingos <lucasdomingos07@gmail.com>
2025-12-03 14:24:35 +0000

    Na ultima frase da pagina quebre a linha:
    Ao inves de:
    "Material digita

--- src/components/landing/footer.tsx
+++ src/components/landing/footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
     <footer className="py-8 bg-background">
       <div className="container mx-auto text-center text-xs text-gray-400 space-y-2">
         <p>&copy; {currentYear} Colorindo Fé e Alegria | Todos os direitos reservados.</p>
-        <p>Material digital de educação cristã para uso pessoal e educativo.</p>
+        <p>Material digital de educação cristã<br />para uso pessoal e educativo.</p>
       </div>
     </footer>
   );
