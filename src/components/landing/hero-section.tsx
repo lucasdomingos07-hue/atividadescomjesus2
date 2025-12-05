fb95833034e3b8fc28d8dbf1e819bd589234c605
Lucas Domingos <lucasdomingos07@gmail.com>
2025-12-05 12:23:50 +0000

    Deixe o "a" da headline em negrito, o "a" que está antres de "palavra"

--- src/components/landing/hero-section.tsx
+++ src/components/landing/hero-section.tsx
@@ -9,9 +9,9 @@ export default function HeroSection() {
     <section className="relative pb-4 bg-background">
       <div className="bg-brand-green pt-8 pb-56 md:pb-64">
         <div className="container mx-auto px-4 md:px-6 flex flex-col items-center relative z-10">
-          <h1 className="text-3xl md:text-[2.65rem] tracking-tight text-center text-primary-foreground leading-tight">
+          <h1 className="text-3xl md:text-4xl tracking-tight text-center text-primary-foreground leading-tight">
             Ensine ao seu filho<br />
-            a <strong>palavra de Deus</strong><br />
+            <strong>a</strong> <strong>palavra de Deus</strong><br />
             de forma divertida!
           </h1>
         </div>
