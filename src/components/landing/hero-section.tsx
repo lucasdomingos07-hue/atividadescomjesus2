82de1106115acd786e903a6f2b65eb174072506e
Lucas Domingos <lucasdomingos07@gmail.com>
2025-12-05 12:18:54 +0000

    desfaça as 2 ultimas alterações

--- src/components/landing/hero-section.tsx
+++ src/components/landing/hero-section.tsx
@@ -7,15 +7,15 @@ export default function HeroSection() {
 
   return (
     <section className="relative pb-4 bg-background">
-      <div className="bg-brand-green pt-8 pb-32">
+      <div className="bg-brand-green pt-10 pb-48">
         <div className="container mx-auto px-4 md:px-6 flex flex-col items-center relative z-10">
-          <h1 className="text-3xl md:text-[2.5rem] tracking-tight text-center text-primary-foreground mb-4 leading-tight">
+          <h1 className="text-3xl md:text-[2.65rem] tracking-tight text-center text-primary-foreground mb-4 leading-tight">
             Ensine ao seu filho<br />a <strong className="font-bold">palavra de Deus</strong><br />
             de forma divertida!
           </h1>
         </div>
       </div>
-      <div className="container mx-auto px-4 md:px-6 flex flex-col items-center relative z-10 -mt-[14.5rem] pt-4">
+      <div className="container mx-auto px-4 md:px-6 flex flex-col items-center relative z-10 -mt-[15.5rem] pt-4">
 
         {/* Video */}
         <div className="w-full max-w-lg mt-4">
