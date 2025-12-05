27009f68a88c2a014d8d12e78e0c02ed14740c8d
Lucas Domingos <lucasdomingos07@gmail.com>
2025-12-05 12:12:16 +0000

    suba levemente um pouco a headline

--- src/components/landing/hero-section.tsx
+++ src/components/landing/hero-section.tsx
@@ -7,15 +7,15 @@ export default function HeroSection() {
 
   return (
     <section className="relative pb-4 bg-background">
-      <div className="bg-brand-green pt-12 pb-32">
+      <div className="bg-brand-green pt-8 pb-32">
         <div className="container mx-auto px-4 md:px-6 flex flex-col items-center relative z-10">
-          <h1 className="text-3xl md:text-3xl font-headline tracking-tight text-center text-primary-foreground mb-6">
+          <h1 className="text-3xl md:text-[2.2rem] font-headline tracking-tight text-center text-primary-foreground mb-4 leading-tight">
             Ensine ao seu filho<br />a <strong className="font-bold">palavra de Deus</strong><br />
             de forma divertida!
           </h1>
         </div>
       </div>
-      <div className="container mx-auto px-4 md:px-6 flex flex-col items-center relative z-10 -mt-[180px] pt-4">
+      <div className="container mx-auto px-4 md:px-6 flex flex-col items-center relative z-10 -mt-40 pt-2">
 
         {/* Video */}
         <div className="w-full max-w-lg mt-4">
