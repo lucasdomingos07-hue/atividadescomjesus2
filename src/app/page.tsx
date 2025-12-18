58d3d37425743b424f93d3c2c2d1af9610d540ea
Lucas Domingos <lucasdomingos07@gmail.com>
2025-12-18 20:24:59 +0000

    Agora coloque a sessao "Atividades completas para aprender sobre Jesus!"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,8 +15,8 @@ export default function Home() {
     <div className="flex flex-col min-h-screen bg-background">
       <main className="flex-1">
         <HeroSection />
-        <BenefitsSection />
         <ColoringPagesSection />
+        <BenefitsSection />
         <TestimonialsSection />
         <CreatorSection />
         <WhatYouGetSection />
