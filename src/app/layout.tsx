551b861c66baddce1f60aa96c49f4a72b046bc49
Lucas Domingos <lucasdomingos07@gmail.com>
2025-12-19 00:27:17 +0000

    Verifique pois o pixel nao esta sendo identificado na minha pagina.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type {Metadata} from 'next';
 import './globals.css';
 import { cn } from "@/lib/utils";
 import { Toaster } from "@/components/ui/toaster"
-import Script from 'next/script';
 
 export const metadata: Metadata = {
   title: 'Colorindo Fé e Alegria',
@@ -20,16 +19,18 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&family=Poppins:wght@600;700&display=swap" rel="stylesheet" />
-        <Script id="utmify-pixel" strategy="afterInteractive">
-          {`
-            window.pixelId = "6944986d50b06f8216e83e63";
-            var a = document.createElement("script");
-            a.setAttribute("async", "");
-            a.setAttribute("defer", "");
-            a.setAttribute("src", "https://cdn.utmify.com.br/scripts/pixel/pixel.js");
-            document.head.appendChild(a);
-          `}
-        </Script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.pixelId = "6944986d50b06f8216e83e63";
+              var a = document.createElement("script");
+              a.setAttribute("async", "");
+              a.setAttribute("defer", "");
+              a.setAttribute("src", "https://cdn.utmify.com.br/scripts/pixel/pixel.js");
+              document.head.appendChild(a);
+            `,
+          }}
+        />
         <script
           src="https://cdn.utmify.com.br/scripts/utms/latest.js"
           data-utmify-prevent-xcod-sck
